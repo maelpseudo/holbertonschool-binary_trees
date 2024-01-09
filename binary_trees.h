0d562d2f9e3403bd7e9d6f556e12bf0e804e4b2b
maelpseudo <mael.cuny@gmail.com>
2024-01-09 14:46:18 +0100

    Updated header file and added 4th task code

--- binary_trees.h
+++ binary_trees.h
@@ -19,5 +19,6 @@ typedef struct binary_tree_s
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
