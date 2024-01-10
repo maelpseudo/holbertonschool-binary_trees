f50022c2ae437ce85df4e23f1268e2b88f87363c
maelpseudo <mael.cuny@gmail.com>
2024-01-10 16:13:40 +0100

    Modified Task 12 and 16

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,18 +3,16 @@
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves
- * 
+ *
  * Return: number of leaves in the tree, 0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+        if (tree == NULL)
+                return 0;
 
-    // Check if the current node is a leaf
-    if (tree->left == NULL && tree->right == NULL)
-        return 1;
+        if (tree->left == NULL && tree->right == NULL)
+                return 1;
 
-    // Recursively count the leaves in the left and right subtrees
-    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+        return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }
