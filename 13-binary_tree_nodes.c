c1cfbd7fb09600f29bb25a101e4406aa9bb9f143
maelpseudo <mael.cuny@gmail.com>
2024-01-10 15:54:14 +0100

    Added all files for Task 13 and modified header file

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * @tree: Pointer to the root node of the tree to measure the nodes
+ * 
+ * Return: Number of nodes with at least 1 child, 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+
+    // Count the current node only if it has at least one child
+    size_t count = (tree->left != NULL || tree->right != NULL) ? 1 : 0;
+
+    // Recursively count nodes in the left and right subtrees
+    count += binary_tree_nodes(tree->left);
+    count += binary_tree_nodes(tree->right);
+
+    return count;
+}
