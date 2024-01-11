468ef52f46c72fedce9e3df6539a3e75ef35f20a
Alexey Chernov <alexchernov063@gmail.com>
2024-01-11 03:07:46 -0800

    betty correction

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,6 +4,8 @@
  * binary_tree_is_root - checks if a given node is a root
  *
  * @node: node
+ *
+ * Return: 1 if node is a root, otherwise 0
 */
 
 int binary_tree_is_root(const binary_tree_t *node)
