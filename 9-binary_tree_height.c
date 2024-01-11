58a1cb9a5c4df62807a587efdfdb643adc9f1042
Alexey Chernov <alexchernov063@gmail.com>
2024-01-11 03:04:32 -0800

    betty correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a binary tree
  *
+ * @tree: a pointer to the tree
  *
- *
- *
- *
+ * Return: height of the node, 0 if tree is NULL
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
