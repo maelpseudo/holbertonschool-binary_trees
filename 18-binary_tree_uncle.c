103244c20ee23718612af76e24966f756664a2b8
maelpseudo <mael.cuny@gmail.com>
2024-01-11 15:58:09 +0100

    Added task 18 files and modified header file

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,18 +6,19 @@
  *
  * Return: pointer to the uncle node, or NULL if node is NULL or has no uncle
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    binary_tree_t *parent, *grandparent;
+        binary_tree_t *parent, *grandparent;
 
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+        if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+                return (NULL);
 
-    parent = node->parent;
-    grandparent = parent->parent;
+        parent = node->parent;
+        grandparent = parent->parent;
 
-    if (grandparent->left == parent)
-        return (grandparent->right);
-    else
-        return (grandparent->left);
+        if (grandparent->left == parent)
+                return (grandparent->right);
+        else
+                return (grandparent->left);
 }
